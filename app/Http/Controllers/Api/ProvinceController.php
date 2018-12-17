d5764d35fd9a55cb7da1688c7b5507f5e133b301
quannh <quannh95@gmail.com>
2018-12-17 09:53:54 +0700

    api register, login, getdata

---
+++ app/Http/Controllers/Api/ProvinceController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use DB;
+
+class ProvinceController extends Controller
+{
+	public function getprovince(Request $request)
+	{
+		$size = $request->size;
+	    $area_id = $request->area_id;
+		if(!id_null($size)){
+			if(!id_null($area_id)){
+				return response()->json([
+		    		'code' => 0, 
+		    		'data' => DB::table('provinces')
+		    					->select(['id', 'name','area_id'])
+		    					->where('area_id',$area_id)
+								->paginate($size)
+				], 200);
+			}else{
+				return response()->json([
+		    		'code' => 0, 
+		    		'data' => DB::table('provinces')
+		    					->select(['id', 'name','area_id'])
+								->paginate($size)
+				], 200);
+			}
+		}else{
+			return response()->json([
+	    		'code' => 0, 
+	    		'data' => DB::table('provinces')
+	    					->select(['id', 'name','area_id'])
+							->paginate()
+			], 200);
+		}
+	}
+    
+}
