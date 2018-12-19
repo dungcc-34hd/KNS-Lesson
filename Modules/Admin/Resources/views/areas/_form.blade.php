99717f02f4b44a0e180293051301ef0d21413ad7
dungcc <chuchungdung97@gmail.com>
2018-12-19 17:41:20 +0700

    fix merge tree area province

--- Modules/Admin/Resources/views/areas/_form.blade.php
+++ Modules/Admin/Resources/views/areas/_form.blade.php
@@ -1,21 +1,22 @@
 <div class="box-body">
-<div class="row">
-    <div class="col-md-6">
-        <div class="form-group">
-            <label>Tên khu vực @include('common.require')</label>
-            <div class="clearfix">
-                <input type="text" class="form-control" name="name"
-                       value="@isset($area){{$area->name}}@endisset">
-                @if($errors)
-                    <span style="color: #dd4b39;" class="text-danger">{{$errors->first('name')}}</span> 
-                @endif
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label>Tên khu vực @include('common.require')</label>
+                <div class="clearfix">
+                    <input type="text" class="form-control" name="name"
+                           value="@isset($area){{$area->name}}@endisset">
+                    @if($errors)
+                        <span style="color: #dd4b39;" class="text-danger">{{$errors->first('name')}}</span>
+                    @endif
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label>Mô tả @include('common.require')</label>
-            <div class="clearfix">
-                <input type="text" class="form-control" name="description"
-                       value="@isset($area){{$area->description}}@endisset">
+            <div class="form-group">
+                <label>Mô tả @include('common.require')</label>
+                <div class="clearfix">
+                    <input type="text" class="form-control" name="description"
+                           value="@isset($area){{$area->description}}@endisset">
+                </div>
             </div>
         </div>
     </div>
