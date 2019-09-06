818d1a04fa0a364124ec3d9ac018abe2d1256d96
dungcc-34hd <30853928+dungcc-34hd@users.noreply.github.com>
2019-09-06 16:55:56 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,3 +26,29 @@ class HomeController extends Controller
         return view('home');
     }
 }
+public function updateOrderLesson($id, $start, $stop)
+    {
+        if ($start < $stop) {
+            $orderLessonDetail = LessonDetail::where('lesson_id', $id)->where('order', '>=', $start)->where('order', '<=', $stop)->orderBy('order')->get();
+            foreach ($orderLessonDetail as $key => $item) {
+                if ($item->order == $start) {
+                    $item->order = $stop;
+                } else {
+                    $item->order--;
+                }
+                $item->save();
+
+            }
+        } else {
+            $orderLessonDetail = LessonDetail::where('lesson_id', $id)->where('order', '>=', $stop)->where('order', '<=', $start)->orderBy('order')->get();
+            foreach ($orderLessonDetail as $key => $item) {
+                if ($item->order == $start) {
+                    $item->order = $stop;
+                } else {
+                    $item->order++;
+                }
+                $item->save();
+
+            }
+        }
+    }
